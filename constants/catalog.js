9fb6d7bac5ad53f3fde074bed8405f382d7eaa85
Mikha <elanov1991@gmail.com>
2023-08-31 09:16:46 +0700

    Add catalog

---
+++ constants/catalog.js
@@ -0,0 +1,20 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'EPIPHONE LP STANDARD 60S',
+        img: 'img/catalog/EPIPHONE.jpg',
+        price: 21600,
+    },
+    {
+        id: 'el2',
+        name: 'EPIPHONE SG STANDARD',
+        img: 'img/catalog/EPIPHONE SG.jpg',
+        price: 16200,
+    },
+    {
+        id: 'el3',
+        name: 'FENDER PLAYER SERIES STRATOCASTER PF ELECTRIC GUITAR',
+        img: 'img/catalog/FENDER.jpg',
+        price: 28800,
+    },
+];
