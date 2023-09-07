2c77d1d5e85b1254e663a3bbcacd3d8b962f0c0c
Mikha <elanov1991@gmail.com>
2023-09-07 12:46:23 +0700

    add header with count products

---
+++ components/header/header.js
@@ -0,0 +1,20 @@
+class Header {
+
+    render(count) {
+        const html = `
+        <div class="header-container">
+            <div class="header-counter">
+                🔥 ${count}
+            </div>
+        </div>
+        `
+
+        ROOT_HEADER.innerHTML = html
+    }
+
+}
+
+const headerPage = new Header()
+
+const productsStore = localStorageUtill.getProducts()
+headerPage.render(productsStore.length)
